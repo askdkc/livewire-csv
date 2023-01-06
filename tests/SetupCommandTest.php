05e654bc0b50b5c179d68149e0b699c1cf08c816
dkc <askdkc@users.noreply.github.com>
2023-01-06 21:30:47 +0900

    Add New Tests

--- tests/SetupCommandTest.php
+++ tests/SetupCommandTest.php
@@ -5,17 +5,38 @@ it('runs setup command without error', function () {
     $this->artisan('livecsv-setup')
         ->expectsOutput('Preparing Livewire-CSV necessary migrations / 必要なマイグレーションを準備します')
         ->expectsOutput('Publishing migration... / マイグレーションファイル準備中')
+        ->expectsConfirmation('Would you like to have Japanese Translation files? / 日本語化ファイルが必要ですか?', false)
         ->expectsConfirmation('Would you like to run the migrations now? / マイグレーションを実行しますか?', 'no')
         ->expectsOutput('Done! / 完了!')
         ->expectsConfirmation("Would you like to star our repo on GitHub? \n GitHubリポジトリにスターの御協力をお願いします🙏", 'no')
         ->assertExitCode(0);
 });
 
-it('sees migration and lang files after running setup command', function () {
+it('sees migration files but not Japanese Translation files after running setup command', function () {
 
     $this->artisan('livecsv-setup')
         ->expectsOutput('Preparing Livewire-CSV necessary migrations / 必要なマイグレーションを準備します')
         ->expectsOutput('Publishing migration... / マイグレーションファイル準備中')
+        ->expectsConfirmation('Would you like to have Japanese Translation files? / 日本語化ファイルが必要ですか?', false)
+        ->expectsConfirmation('Would you like to run the migrations now? / マイグレーションを実行しますか?', 'no')
+        ->expectsOutput('Done! / 完了!')
+        ->expectsConfirmation("Would you like to star our repo on GitHub? \n GitHubリポジトリにスターの御協力をお願いします🙏", 'no')
+        ->assertExitCode(0);
+
+    $this->assertTrue($this->migrationExists('csv_imports_table'));
+    $this->assertTrue($this->migrationExists('create_jobs_table'));
+    $this->assertTrue($this->migrationExists('create_csv_imports_table'));
+    $this->assertFileExists(config_path('livewire_csv.php'));
+    $this->assertFileDoesNotExist(lang_path('ja/validation.php'));
+    $this->assertFileDoesNotExist(lang_path('ja.json'));
+});
+
+it('sees migration files and Japanese Translation files after running setup command', function () {
+
+    $this->artisan('livecsv-setup')
+        ->expectsOutput('Preparing Livewire-CSV necessary migrations / 必要なマイグレーションを準備します')
+        ->expectsOutput('Publishing migration... / マイグレーションファイル準備中')
+        ->expectsConfirmation('Would you like to have Japanese Translation files? / 日本語化ファイルが必要ですか?', 'yes')
         ->expectsConfirmation('Would you like to run the migrations now? / マイグレーションを実行しますか?', 'no')
         ->expectsOutput('Done! / 完了!')
         ->expectsConfirmation("Would you like to star our repo on GitHub? \n GitHubリポジトリにスターの御協力をお願いします🙏", 'no')
